4d58ed6122df18ad4b3624fb9922c6bbee67254a
hafid abi d <aselole87@gmail.com>
2024-09-01 06:48:38 +0700

    add logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -131,7 +131,7 @@ const App = (props: ExtendedAppProps) => {
           <title>{`${themeConfig.templateName} - Merangkul karya, mengglobal bersama`}</title>
           <meta
             name="description"
-            content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+            content={`${themeConfig.templateName} – Merangkul karya, mengglobal bersama.`}
           />
           <meta name="keywords" content="Merangkul karya, mengglobal bersama" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -1,118 +1,138 @@
 // ** React Imports
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
 // ** Next Import
-import Link from 'next/link'
+import Link from "next/link";
 
 // ** MUI Components
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import Box, { BoxProps } from '@mui/material/Box'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { styled, useTheme } from '@mui/material/styles'
-import Typography, { TypographyProps } from '@mui/material/Typography'
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box, { BoxProps } from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { styled, useTheme } from "@mui/material/styles";
+import Typography, { TypographyProps } from "@mui/material/Typography";
 
 // ** Icon Imports
-import Icon from 'src/@core/components/icon'
+import Icon from "src/@core/components/icon";
 
 // ** Configs
-import themeConfig from 'src/configs/themeConfig'
+import themeConfig from "src/configs/themeConfig";
 
 // ** Layout Import
-import BlankLayout from 'src/@core/layouts/BlankLayout'
+import BlankLayout from "src/@core/layouts/BlankLayout";
 
 // ** Hooks
-import { useSettings } from 'src/@core/hooks/useSettings'
+import { useSettings } from "src/@core/hooks/useSettings";
 
 // ** Demo Imports
-import FooterIllustrationsV2 from 'src/views/pages/auth/FooterIllustrationsV2'
+import FooterIllustrationsV2 from "src/views/pages/auth/FooterIllustrationsV2";
 
 // Styled Components
-const ForgotPasswordIllustrationWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  padding: theme.spacing(20),
-  paddingRight: '0 !important',
-  [theme.breakpoints.down('lg')]: {
-    padding: theme.spacing(10)
-  }
-}))
+const ForgotPasswordIllustrationWrapper = styled(Box)<BoxProps>(
+  ({ theme }) => ({
+    padding: theme.spacing(20),
+    paddingRight: "0 !important",
+    [theme.breakpoints.down("lg")]: {
+      padding: theme.spacing(10),
+    },
+  })
+);
 
-const ForgotPasswordIllustration = styled('img')(({ theme }) => ({
-  maxWidth: '48rem',
-  [theme.breakpoints.down('xl')]: {
-    maxWidth: '38rem'
+const ForgotPasswordIllustration = styled("img")(({ theme }) => ({
+  maxWidth: "48rem",
+  [theme.breakpoints.down("xl")]: {
+    maxWidth: "38rem",
   },
-  [theme.breakpoints.down('lg')]: {
-    maxWidth: '30rem'
-  }
-}))
+  [theme.breakpoints.down("lg")]: {
+    maxWidth: "30rem",
+  },
+}));
 
 const RightWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  width: '100%',
-  [theme.breakpoints.up('md')]: {
-    maxWidth: 400
+  width: "100%",
+  [theme.breakpoints.up("md")]: {
+    maxWidth: 400,
   },
-  [theme.breakpoints.up('lg')]: {
-    maxWidth: 450
-  }
-}))
+  [theme.breakpoints.up("lg")]: {
+    maxWidth: 450,
+  },
+}));
 
 const BoxWrapper = styled(Box)<BoxProps>(({ theme }) => ({
-  width: '100%',
-  [theme.breakpoints.down('md')]: {
-    maxWidth: 400
-  }
-}))
+  width: "100%",
+  [theme.breakpoints.down("md")]: {
+    maxWidth: 400,
+  },
+}));
 
 const TypographyStyled = styled(Typography)<TypographyProps>(({ theme }) => ({
   fontWeight: 600,
-  letterSpacing: '0.18px',
+  letterSpacing: "0.18px",
   marginBottom: theme.spacing(1.5),
-  [theme.breakpoints.down('md')]: { marginTop: theme.spacing(8) }
-}))
+  [theme.breakpoints.down("md")]: { marginTop: theme.spacing(8) },
+}));
 
 const LinkStyled = styled(Link)(({ theme }) => ({
-  display: 'flex',
-  '& svg': { mr: 1.5 },
-  alignItems: 'center',
-  textDecoration: 'none',
-  justifyContent: 'center',
-  color: theme.palette.primary.main
-}))
+  display: "flex",
+  "& svg": { mr: 1.5 },
+  alignItems: "center",
+  textDecoration: "none",
+  justifyContent: "center",
+  color: theme.palette.primary.main,
+}));
 
 const ForgotPassword = () => {
   // ** Hooks
-  const theme = useTheme()
-  const { settings } = useSettings()
+  const theme = useTheme();
+  const { settings } = useSettings();
 
   // ** Vars
-  const { skin } = settings
-  const hidden = useMediaQuery(theme.breakpoints.down('md'))
+  const { skin } = settings;
+  const hidden = useMediaQuery(theme.breakpoints.down("md"));
 
   const imageSource =
-    skin === 'bordered' ? 'auth-v2-forgot-password-illustration-bordered' : 'auth-v2-forgot-password-illustration'
+    skin === "bordered"
+      ? "auth-v2-forgot-password-illustration-bordered"
+      : "auth-v2-forgot-password-illustration";
 
   return (
-    <Box className='content-right'>
+    <Box className="content-right">
       {!hidden ? (
-        <Box sx={{ flex: 1, display: 'flex', position: 'relative', alignItems: 'center', justifyContent: 'center' }}>
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            position: "relative",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <ForgotPasswordIllustrationWrapper>
             <ForgotPasswordIllustration
-              alt='forgot-password-illustration'
+              alt="forgot-password-illustration"
               src={`/images/pages/${imageSource}-${theme.palette.mode}.png`}
             />
           </ForgotPasswordIllustrationWrapper>
-          <FooterIllustrationsV2 image={`/images/pages/auth-v2-forgot-password-mask-${theme.palette.mode}.png`} />
+          <FooterIllustrationsV2
+            image={`/images/pages/auth-v2-forgot-password-mask-${theme.palette.mode}.png`}
+          />
         </Box>
       ) : null}
-      <RightWrapper sx={skin === 'bordered' && !hidden ? { borderLeft: `1px solid ${theme.palette.divider}` } : {}}>
+      <RightWrapper
+        sx={
+          skin === "bordered" && !hidden
+            ? { borderLeft: `1px solid ${theme.palette.divider}` }
+            : {}
+        }
+      >
         <Box
           sx={{
             p: 7,
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'background.paper'
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "background.paper",
           }}
         >
           <BoxWrapper>
@@ -120,100 +140,63 @@ const ForgotPassword = () => {
               sx={{
                 top: 30,
                 left: 40,
-                display: 'flex',
-                position: 'absolute',
-                alignItems: 'center',
-                justifyContent: 'center'
+                display: "flex",
+                position: "absolute",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              <svg width={47} fill='none' height={26} viewBox='0 0 268 150' xmlns='http://www.w3.org/2000/svg'>
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 195.571 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint0_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 196.084 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 173.147 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fillOpacity='0.4'
-                  fill='url(#paint1_linear_7821_79167)'
-                  transform='matrix(-0.865206 0.501417 0.498585 0.866841 94.1973 0)'
-                />
-                <rect
-                  rx='25.1443'
-                  width='50.2886'
-                  height='143.953'
-                  fill={theme.palette.primary.main}
-                  transform='matrix(0.865206 0.501417 -0.498585 0.866841 71.7728 0)'
-                />
-                <defs>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint0_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
-                  >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                  <linearGradient
-                    y1='0'
-                    x1='25.1443'
-                    x2='25.1443'
-                    y2='143.953'
-                    id='paint1_linear_7821_79167'
-                    gradientUnits='userSpaceOnUse'
-                  >
-                    <stop />
-                    <stop offset='1' stopOpacity='0' />
-                  </linearGradient>
-                </defs>
-              </svg>
-              <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+              <img alt="logo" src="/images/icon.png" width={40} height={40} />
+              <Typography
+                variant="h6"
+                sx={{
+                  ml: 2,
+                  lineHeight: 1,
+                  fontWeight: 700,
+                  fontSize: "1.5rem !important",
+                }}
+              >
                 {themeConfig.templateName}
               </Typography>
             </Box>
             <Box sx={{ mb: 6 }}>
-              <TypographyStyled variant='h5'>Forgot Password? 🔒</TypographyStyled>
-              <Typography variant='body2'>
-                Enter your email and we&prime;ll send you instructions to reset your password
+              <TypographyStyled variant="h5">
+                Forgot Password? 🔒
+              </TypographyStyled>
+              <Typography variant="body2">
+                Enter your email and we&prime;ll send you instructions to reset
+                your password
               </Typography>
             </Box>
-            <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-              <TextField autoFocus type='email' label='Email' sx={{ display: 'flex', mb: 4 }} />
-              <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 5.25 }}>
+            <form
+              noValidate
+              autoComplete="off"
+              onSubmit={(e) => e.preventDefault()}
+            >
+              <TextField
+                autoFocus
+                type="email"
+                label="Email"
+                sx={{ display: "flex", mb: 4 }}
+              />
+              <Button
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+                sx={{ mb: 5.25 }}
+              >
                 Send reset link
               </Button>
-              <Typography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <LinkStyled href='/login'>
-                  <Icon icon='mdi:chevron-left' fontSize='2rem' />
+              <Typography
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <LinkStyled href="/login">
+                  <Icon icon="mdi:chevron-left" fontSize="2rem" />
                   <span>Back to login</span>
                 </LinkStyled>
               </Typography>
@@ -222,10 +205,12 @@ const ForgotPassword = () => {
         </Box>
       </RightWrapper>
     </Box>
-  )
-}
+  );
+};
 
-ForgotPassword.guestGuard = true
-ForgotPassword.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+ForgotPassword.guestGuard = true;
+ForgotPassword.getLayout = (page: ReactNode) => (
+  <BlankLayout>{page}</BlankLayout>
+);
 
-export default ForgotPassword
+export default ForgotPassword;
